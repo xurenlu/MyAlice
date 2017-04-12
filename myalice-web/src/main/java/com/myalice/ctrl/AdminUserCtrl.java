da460ddcc26c9b50055cd5f31aa8c6afd7ba6fbc
hou <834865081@qq.com>
2017-04-12 17:31:34 +0800

    AdminUserCtrl

--- myalice-web/src/main/java/com/myalice/ctrl/AdminUserCtrl.java
+++ myalice-web/src/main/java/com/myalice/ctrl/AdminUserCtrl.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminUserCtrl {
 
-	@RequestMapping("/admin/list")
+	@RequestMapping("/list")
 	public String list() {
 		return "redirect:/admin/index.html";
 	}
