662af91c6f0acaddec2d55b92945b2873507c755
1837307557@qq.com <1837307557@qq.com>
2017-04-18 16:20:00 +0800

    提交切面操作ES的测试类

--- myalice-service/src/main/java/com/myalice/config/ElasticsearchClient.java
+++ myalice-service/src/main/java/com/myalice/config/ElasticsearchClient.java
@@ -1,8 +1,5 @@
 package com.myalice.config;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
@@ -10,6 +7,9 @@ import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Configuration
 public class ElasticsearchClient {
@@ -22,20 +22,19 @@ public class ElasticsearchClient {
     private String cluster_name;
     @Value("${cluster-port}")
     private Integer cluster_port;
+    @Value("${elasticsearch.enabled}")
+    private boolean elasticsearchEnabled;
 
     @Bean
     public TransportClient transportClient() throws UnknownHostException {
-        Settings settings = Settings.builder().put(cluster_name, elasticsearch).build();
-        TransportClient client = new PreBuiltTransportClient(settings)
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(cluster_nodes), cluster_port));
+        TransportClient client = null;
+        if (elasticsearchEnabled) {
+            Settings settings = Settings.builder().put(cluster_name, elasticsearch).build();
+            client = new PreBuiltTransportClient(settings)
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(cluster_nodes), cluster_port));
+        }
         return client;
     }
-
-
-
-
-
-
 
 
 }
