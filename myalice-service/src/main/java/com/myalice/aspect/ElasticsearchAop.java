5faa3214a28b289004618a9c4aa955372640ed65
garyHuang <835865081@qq.com>
2017-05-06 16:32:21 +0800

    aop 关闭es client

--- myalice-service/src/main/java/com/myalice/aspect/ElasticsearchAop.java
+++ myalice-service/src/main/java/com/myalice/aspect/ElasticsearchAop.java
@@ -13,23 +13,19 @@ import com.myalice.config.ElasticsearchProporties;
 @Component
 @Aspect
 public class ElasticsearchAop {
-    Logger logger = LoggerFactory.getLogger(ElasticsearchAop.class);
-    
+	Logger logger = LoggerFactory.getLogger(ElasticsearchAop.class);
 
 	@Autowired
 	protected ElasticsearchProporties elasticsearchProporties;
-   
-    @After("execution(* com.myalice.es.IElasticsearch.*(..)) ")
-    public void doAfter() throws Throwable {
-    	TransportClient transportClient = elasticsearchProporties.getTransportClient(); 
-    	if(null != transportClient ){
-    		try {
-    			System.out.println("close");
-    			transportClient.close();
-			} catch (Exception e) { 
+
+	@After("execution(* com.myalice.es.IElasticsearch.*(..)) ")
+	public void doAfter() throws Throwable {
+		TransportClient transportClient = elasticsearchProporties.getTransportClient();
+		if (null != transportClient) {
+			try {
+				transportClient.close();
+			} catch (Exception e) {
 			}
-    	}
-    }
-
-
+		}
+	}
 }
