fc1bd150a97f927ad17b099b51d63ba2442c911f
garyHuang <835865081@qq.com>
2017-05-06 16:31:47 +0800

    新增问题库servcie

--- myalice-service/src/main/java/com/myalice/services/ESQuestionService.java
+++ myalice-service/src/main/java/com/myalice/services/ESQuestionService.java
@@ -1,15 +1,48 @@
 package com.myalice.services;
 
-import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
-import com.myalice.es.IElasticsearch;
+import com.myalice.config.ElasticsearchProporties;
+import com.myalice.domain.ElasticsearchData;
+import com.myalice.es.impl.ElasticsearchService;
 
 @Service
 public class ESQuestionService {
-	
-	@Resource(name="questionEs")
-	IElasticsearch questionEs ;
-	
+
+	protected ElasticsearchProporties elasticsearchProporties;
+
+	protected ElasticsearchService elasticsearchService;
+
+	public ESQuestionService(ElasticsearchProporties elasticsearchProporties) {
+		this.elasticsearchProporties = elasticsearchProporties;
+		elasticsearchService = new ElasticsearchService("myalice", "question");
+		elasticsearchService.setElasticsearchProporties(elasticsearchProporties);
+	}
+
+	public boolean add(Map<String, Object> data) {
+		return elasticsearchService.add(data);
+	}
+
+	public boolean adds(List<Map<String, Object>> datas) {
+		return elasticsearchService.adds(datas);
+	}
+
+	public boolean remove(String id) {
+		return elasticsearchService.remove(id);
+	}
+
+	public boolean removes(String... ids) {
+		return elasticsearchService.removes(ids);
+	}
+
+	public void query(ElasticsearchData searchData) {
+		elasticsearchService.query(searchData);
+	}
+
+	public Map<String, Object> get(String id) {
+		return elasticsearchService.get(id);
+	}
 }
