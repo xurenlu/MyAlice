197a27c3cdf6611789d525e2c7efd27d00134e43
garyHuang <835865081@qq.com>
2017-05-20 22:16:27 +0800

    优化

--- myalice-web/src/main/java/com/myalice/ctrl/WebSocketController.java
+++ myalice-web/src/main/java/com/myalice/ctrl/WebSocketController.java
@@ -13,7 +13,10 @@ import org.springframework.stereotype.Controller;
 import com.alibaba.fastjson.JSON;
 import com.myalice.beans.Message;
 import com.myalice.beans.Response;
+import com.myalice.domain.TalkRecord;
 import com.myalice.services.ESQuestionService;
+import com.myalice.services.TalkRecordService;
+import com.myalice.utils.MyAliceUtils;
 import com.myalice.utils.Tools;
 
 @Controller
@@ -24,6 +27,9 @@ public class WebSocketController {
 	
 	@Autowired
 	protected ESQuestionService questionService ;
+	
+	@Autowired
+	protected TalkRecordService talkRecordService;
 	
 	@MessageMapping("/welcome")
 	@SendTo("/topic/getResponse")
@@ -37,30 +43,38 @@ public class WebSocketController {
 	public void handleChat(Principal principal, Message message) {
 		
 		Map<String, Object> answer = questionService.searchAnswer(message.getName()) ;
-		
+		TalkRecord record = new TalkRecord();
 		{
 			Map<String,Object> responseMsg = new HashMap<>() ;
 			responseMsg.put("date", Tools.currentDate());
 			responseMsg.put("clazz", "even");
 			responseMsg.put("name", principal.getName() ); 
 			responseMsg.put("anwser", message.getName());
+			record.setContent( message.getName() ); ;
 			messagingTemplate.convertAndSendToUser(principal.getName()
 					, "/queue/notifications",JSON.toJSONStringWithDateFormat(responseMsg, "yyyy-MM-dd HH:mm:ss") );
 		}
-		
+		record.setUserType("");
+		record.setConnectionId("");
 		Map<String,Object> responseMsg = new HashMap<>() ;
 		responseMsg.put("date", Tools.currentDate());
 		responseMsg.put("clazz", "odd");
 		responseMsg.put("name", "系统管理员" );
 		if(null != answer){
+			record.setReplyType(1);
 			responseMsg.put("anwser", answer.get("anwser"));
 			messagingTemplate.convertAndSendToUser(principal.getName()
 				, "/queue/notifications",JSON.toJSONStringWithDateFormat(responseMsg, "yyyy-MM-dd HH:mm:ss") );
 		}else{
+			record.setReplyType(0);
 			responseMsg.put("anwser", "请重新描述您的问题");
 			messagingTemplate.convertAndSendToUser(principal.getName()
 					, "/queue/notifications",JSON.toJSONStringWithDateFormat(responseMsg, "yyyy-MM-dd HH:mm:ss") );   
 		}
+		
+		record.setReply( MyAliceUtils.toString(responseMsg.get("anwser")));
+		
+		talkRecordService.insert( record ); 
 		/*
 		if (principal.getName().equals("admin")) {
 			// 通过convertAndSendToUser 向用户发送信息,
