e7e025c70366954b1c141d3f6605f1bdb3520486
garyHuang <835865081@qq.com>
2017-05-21 13:27:02 +0800

    聊天记录分页

--- myalice-service/src/main/java/com/myalice/services/TalkRecordService.java
+++ myalice-service/src/main/java/com/myalice/services/TalkRecordService.java
@@ -3,6 +3,8 @@ package com.myalice.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.myalice.domain.TalkRecord;
 import com.myalice.mapping.TalkRecordMapper;
 import com.myalice.utils.Tools;
@@ -17,5 +19,11 @@ public class TalkRecordService {
 		talkRecord.setId(Tools.uuid());
 		talkRecord.setCreateTime(Tools.currentDate());
 		talkRecordMapper.insert(talkRecord);
+	}
+	
+	public Page<TalkRecord> list(int pageId, TalkRecord record) {
+		Page<TalkRecord> startPage = PageHelper.startPage(pageId, 10);
+		talkRecordMapper.select(record) ; 
+		return startPage;
 	}
 }
