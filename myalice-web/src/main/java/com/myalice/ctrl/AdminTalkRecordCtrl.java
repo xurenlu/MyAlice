7b8684ad0a26e522b20471b980f28853313ef5ea
garyHuang <835865081@qq.com>
2017-07-15 18:02:43 +0800

    沙中世界新增导出功能

--- myalice-web/src/main/java/com/myalice/ctrl/AdminTalkRecordCtrl.java
+++ myalice-web/src/main/java/com/myalice/ctrl/AdminTalkRecordCtrl.java
@@ -1,25 +1,84 @@
 package com.myalice.ctrl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageInfo;
 import com.myalice.domain.TalkRecord;
 import com.myalice.services.TalkRecordService;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.streaming.SXSSFCell;
+import org.apache.poi.xssf.streaming.SXSSFRow;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 
 @RestController
 @RequestMapping("/admin/talkrecord")
 public class AdminTalkRecordCtrl {
-	
-	@Autowired
-	protected TalkRecordService talkRecordService ;
-	
-	@RequestMapping("data")
-	public PageInfo<TalkRecord> data(Integer pageNum,TalkRecord record){
-		pageNum = null == pageNum ? 1 : pageNum ;
-		Page<TalkRecord> list = talkRecordService.list(pageNum, record) ;
-		return new PageInfo<>(list) ; 
-	}
+    protected static Logger logger = org.slf4j.LoggerFactory.getLogger("ctrl");
+
+    @Autowired
+    protected TalkRecordService talkRecordService;
+
+    @RequestMapping("data")
+    public PageInfo<TalkRecord> data(Integer pageNum, TalkRecord record) {
+        pageNum = null == pageNum ? 1 : pageNum;
+        Page<TalkRecord> list = talkRecordService.list(pageNum, record);
+        return new PageInfo<>(list);
+    }
+
+    @RequestMapping("data/export")
+    public void exportExcel(TalkRecord record, HttpServletResponse response) {
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String[] headers = new String[]{"咨询人", "提问内容", "创建时间", "回复内容", "状态"};
+            // 声明一个工作薄
+            SXSSFWorkbook workbook = new SXSSFWorkbook(1000);//缓存最大值
+            workbook.setCompressTempFiles(true);
+            SXSSFSheet sheet1 = workbook.createSheet("咨询记录");
+            int excelRow = 0;
+            //标题行
+            SXSSFRow titleRow = sheet1.createRow(excelRow++);
+            for (int i = 0; i < headers.length; i++) {
+                Cell cell = titleRow.createCell(i);
+                cell.setCellValue(headers[i]);
+            }
+
+            for (int pageNum = 1; pageNum < 10000; pageNum++) {
+                Page<TalkRecord> list = talkRecordService.list(pageNum, record);
+                if (list == null || list.size() == 0) {
+                    break;
+                }
+                for (TalkRecord talkRecord : list) {
+                    SXSSFRow row = sheet1.createRow(excelRow++);
+                    SXSSFCell c1 = row.createCell(0);
+                    c1.setCellValue(talkRecord.getUserId());
+                    SXSSFCell c2 = row.createCell(1);
+                    c2.setCellValue(talkRecord.getContent());
+                    SXSSFCell c3 = row.createCell(2);
+                    c3.setCellValue(formatter.format(talkRecord.getCreateTime()));
+                    SXSSFCell c4 = row.createCell(3);
+                    c4.setCellValue(talkRecord.getReply());
+                    SXSSFCell c5 = row.createCell(4);
+                    c5.setCellValue(talkRecord.getReplyType() == 1 ? "已回复" : "未回复");
+                }
+            }
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("Content-disposition", "attachment;filename=talk-record.xlsx");
+            OutputStream ouputStream = response.getOutputStream();
+
+            workbook.write(ouputStream);
+            ouputStream.flush();
+            ouputStream.close();
+            workbook.close();
+        } catch (Exception e) {
+            logger.error("导出文件出错：", e);
+        }
+    }
 }
